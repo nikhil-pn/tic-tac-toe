7d7f542fe7b5c31b4a91a776c4636529b35a407d
nikhilpn725 <nikhilpn725@gmail.com>
2023-01-19 11:09:47 +0530

    server and roomid created

--- server.js
+++ server.js
@@ -2,7 +2,29 @@ const express = require("express");
 
 const app = express();
 const server = require("http").Server(app);
+const PORT = 3001;
+const io = require("socket.io")(server);
+const { v4: uuidv4 } = require("uuid");
 
-server.listen(3001, () => {
+//middlewars
+app.use(express.json());
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("");
+});
+
+app.get("/uuid", (req, res) => {
+  res.redirect("/" + uuidv4());
+});
+
+app.get("/:room", (req, res) => {
+  res.render("room", {
+    roomId: req.params.room,
+  });
+});
+
+server.listen(PORT, () => {
   console.log("server is running at port 3001");
 });
